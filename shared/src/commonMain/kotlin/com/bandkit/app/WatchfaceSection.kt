bf1943e2e762841725f6dd720b05c1fb2fb8f7c7
123IandU <2212977545@qq.com>
2026-07-20 22:47:14 +0800

    perf: add key to WatchfaceSection LazyColumn

--- shared/src/commonMain/kotlin/com/bandkit/app/WatchfaceSection.kt
+++ shared/src/commonMain/kotlin/com/bandkit/app/WatchfaceSection.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -95,8 +96,7 @@ internal fun WatchfaceSection(
             } else {
                 Box(modifier = Modifier.fillMaxWidth().height(240.dp)) {
                     LazyColumn(modifier = Modifier.fillMaxSize()) {
-                        items(watchfaces.size) { index ->
-                            val wf = watchfaces[index]
+                        itemsIndexed(watchfaces, key = { _, wf -> wf.id }) { index, wf ->
                             Row(
                                 modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp),
                                 horizontalArrangement = Arrangement.SpaceBetween,
